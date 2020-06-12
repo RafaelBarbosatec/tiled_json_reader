4800d47cb71b6c035296594466acca5709c0650f
rafaelbarbosatec <rafael.sciensa@MacBook-Pro-de-adminisrador.local>
2020-06-12 11:58:36 -0300

    ler parametro de tile animado

---
+++ lib/tile_set/frame_animation.dart
@@ -0,0 +1,18 @@
+class FrameAnimation {
+  double duration;
+  int tileid;
+
+  FrameAnimation({this.duration, this.tileid});
+
+  FrameAnimation.fromJson(Map<String, dynamic> json) {
+    duration = double.parse(json['duration'].toString());
+    tileid = json['tileid'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['duration'] = this.duration;
+    data['tileid'] = this.tileid;
+    return data;
+  }
+}
--- lib/tile_set/tile_set_item.dart
+++ lib/tile_set/tile_set_item.dart
@@ -1,7 +1,9 @@
+import 'package:tiledjsonreader/tile_set/frame_animation.dart';
 import 'package:tiledjsonreader/tile_set/tile_set_object_group.dart';
 
 class TileSetItem {
   int id;
+  List<FrameAnimation> animation;
   TileSetObjectGroup objectGroup;
 
   TileSetItem({this.id, this.objectGroup});
@@ -11,6 +13,12 @@ class TileSetItem {
     objectGroup = json['objectgroup'] != null
         ? new TileSetObjectGroup.fromJson(json['objectgroup'])
         : null;
+    if (json['animation'] != null) {
+      animation = new List<FrameAnimation>();
+      json['animation'].forEach((v) {
+        animation.add(new FrameAnimation.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
